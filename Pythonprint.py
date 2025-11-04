d74df92b3eda7c2c89565d90614f424d1c26f2e9
Chowa <arfinchowa524@gmail.com>
2025-11-04 23:02:26 +0600

    python print  added

---
+++ Pythonprint.py
@@ -0,0 +1,33 @@
+# Python Output / Print
+# Print Text
+# You have already learned that you can use the print() function to display text or output values:
+
+# ExampleGet your own Python Server
+print("Hello World!")
+
+# You can use the print() function as many times as you want. Each call prints text on a new line by default:
+
+# Example
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+# Double Quotes
+# Text in Python must be inside quotes. You can use either " double quotes or ' single quotes:
+
+# Example
+print("This will work!")
+print('This will also work!')
+
+# If you forget to put the text inside quotes, Python will give an error:
+
+# Example
+# print(This will cause an error)
+# Result:
+
+# SyntaxError: invalid syntax.
+
+# Print Without a New Line
+# By default, the print() function ends with a new line.
+
+# If you want to print multiple words on the same line, you can use the end parameter:
