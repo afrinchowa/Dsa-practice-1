37626a8c457337f14d708b9c5da3d04b1d0c648b
Chowa <arfinchowa524@gmail.com>
2025-11-06 22:37:31 +0600

    python number  added

---
+++ pythonnumber.py
@@ -0,0 +1,21 @@
+# Print Numbers
+# You can also use the print() function to display numbers:
+
+# However, unlike text, we don't put numbers inside double quotes:
+
+# ExampleGet your own Python Server
+print(3)
+print(358)
+print(50000)
+
+# You can also do math inside the print() function:
+
+# Example
+print(3 + 3)
+print(2 * 5)
+
+# Mix Text and Numbers
+# You can combine text and numbers in one output by separating them with a comma:
+
+# Example
+print("I am", 35, "years old.")
