066a1cddb994cdef477b6d6052cad702835864df
Chowa <arfinchowa524@gmail.com>
2025-11-15 09:44:04 +0600

    variable added

---
+++ variable.py
@@ -0,0 +1,23 @@
+# 
+
+# print(type(name))
+
+# age =21
+# age+=1
+# age=age+1
+# print(age)
+# print("Your age is: "+str(age))
+# print(type(age))
+
+
+# height =5.5
+
+# height=height+1
+# print(height)
+# print("Your height is: "+str(height))
+# print(type(height))
+
+
+human =True
+print("Are you a Human: "+str(human))
+print(type(human))
