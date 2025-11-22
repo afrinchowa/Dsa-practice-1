3788673bdcf6f526703830b27416b4cf7c73e517
Chowa <arfinchowa524@gmail.com>
2025-11-22 19:32:35 +0600

    Python Operator

---
+++ Operator.py
@@ -0,0 +1,22 @@
+a=10
+b=20
+
+# addition
+print(a+b)
+# division
+print(b-a)
+#multipication
+print(a*b)
+# division
+print(a/b)
+# modulus %
+print(a%b)
+# Exponentiation
+x=5
+y=2
+print(x**y)  #5*5
+
+# floor division
+sum1 =15
+sum2=2
+print(sum1 //sum2)
