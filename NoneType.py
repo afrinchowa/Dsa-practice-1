87937f3959177387899f8b7c5fca3100a990c3fc
Chowa <arfinchowa524@gmail.com>
2025-11-22 19:22:02 +0600

    python sequence type

---
+++ NoneType.py
@@ -0,0 +1,4 @@
+# none type
+# The None type in Python is used in situations where the absence of a value, or a null value, needs to be explicitly represented. It is a distinct object of type NoneType, and not equivalent to 0, False, or an empty string.
+x=None
+print(type(x))
---
+++ SequenceType.py
@@ -0,0 +1,23 @@
+# list tuple and range
+
+
+# list is look like array
+# list create with third bracket
+# list is mutable
+li=['chowa','noman','anonna','sakib']
+print(li)
+print(type(li))
+li[0]="tuktuki"
+print(li[0])
+# tuple type data
+# tuple is immutable
+tup = (95,6,7,8,90,78)
+print(tup)
+print(type(tup))
+
+#range type data 
+ran= range(61); 
+print(ran)
+
+for i in ran:
+    print(i)
